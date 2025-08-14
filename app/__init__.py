3e169c831854c860c548198e34de035e2858f0aa
MiloPowell <milo.powell@icloud.com>
2025-08-14 09:56:03 -0400

    Error 500

--- app/__init__.py
+++ app/__init__.py
@@ -42,5 +42,10 @@ def create_app(config_class=Config):
     def not_found_error(error):
         return render_template('404.html'), 404
     
+    @app.errorhandler(500)
+    def internal_error(error):
+        db.session.rollback()
+        return render_template('500.html'), 500
+    
 
     return app
